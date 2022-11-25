1950c9f6a790273c5c4c5472525119eade6ce846
aimanfatimaahmed@gmail.com <aimanfatima@sus>
2022-11-25 23:55:46 +0530

    forking and contributing

---
+++ aimanFatima/operators.js
@@ -0,0 +1,10 @@
+console.log(Math.round(151574.954));//nearest number
+console.log(Math.ceil(151574.154));//it will give u next number
+console.log(Math.floor(151574.154));//it will give previous number
+
+console.log(a++);//it will print and increment
+console.log(a);// value + increment
+console.log(++a);//increment and print
+console.log(b--);//print n decrement
+console.log(b);// value - decrement
+console.log(--b);//decrement and print
---
+++ aimanFatima/operators/assing.js
@@ -0,0 +1,9 @@
+zeo values or falsy values
+
+0 
+empty string
+undefined
+null
+
+
+logical not (!) doest give u anyy exp but only operators
